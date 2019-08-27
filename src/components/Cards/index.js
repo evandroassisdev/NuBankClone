18e6b6d7fbe3816f4f76786ac3d92409eed4942e
evandroassisdev <evandroassisdev@gmail.com>
2019-08-27 19:53:59 -0400

    Nubank

---
+++ src/components/Cards/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
+
+export default function Cards() {
+  return(
+    <Container>
+      <Card>
+        <CardHeader>
+          <Icon name="attach-money" size={28} color="#666" />
+          <Icon name="visibility-off" size={28} color="#666" />
+        </CardHeader>
+        <CardContent>
+          <Title>Saldo disponível</Title>
+          <Description>R$ 197.611,65</Description>
+        </CardContent>
+        <CardFooter>
+          <Annotation>
+            Transferência de R$ 20,00 recebida de Jean Zahn hoje ás 06:00h
+          </Annotation>
+        </CardFooter>
+      </Card>
+    </Container>
+  );
+}
